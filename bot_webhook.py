f833e9f6fa6baf74c6bb046946c775e8fbb7365d
LuckCky <www.aivanov@gmail.com>
2017-03-05 23:07:03 +0300

    production v 0.1

--- bot_webhook.py
+++ bot_webhook.py
@@ -22,7 +22,7 @@ WEBHOOK_URL_PATH = "/{}/".format(token)
 bot = telebot.TeleBot(token)
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=["start"])
 def send_welcome(message):
     welcome = 'Привет! Я гороскоп-бот Мерфи. Даю прогноз на день текущий по законам Мерфи. ' \
           'Команды /гороскоп или /horoscope для выдачи гороскопа и ' \
