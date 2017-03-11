8b3b12f4b3e2686502afbe128b021adce969962b
LuckCky <www.aivanov@gmail.com>
2017-03-11 21:38:34 +0300

    some methods deleted

--- prediction.py
+++ prediction.py
@@ -2,14 +2,16 @@ from datetime import datetime
 import random
 
 from signs import signs
-from dbhelper import delete_old_prediction, set_today_prediction
+from dbhelper import set_today_prediction
 from dbhelper import get_today_prediction
+
 
 def line_conter():
     with open('Murpys_laws.txt', 'r') as source:
         for num, line in enumerate(source):
             pass
     return num + 1
+
 
 def predictor():
     lines_num = line_conter()
@@ -20,8 +22,8 @@ def predictor():
         prediction[sign] = lines_nums[i]
     set_today_prediction(datetime.now(), prediction)
 
+
 def read_prediction(sign):
-    delete_old_prediction(datetime.now())
     prediction = get_today_prediction(datetime.now())
     if isinstance(sign, list):
         sign = sign[0]
