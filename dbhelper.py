0528360747fe07045c7fa0f3e619084b6e243d5f
LuckCky <www.aivanov@gmail.com>
2017-03-15 23:16:03 +0300

    debugging

--- dbhelper.py
+++ dbhelper.py
@@ -27,6 +27,7 @@ def set_user_sign(user_id, sign):
     # cursor.execute("INSERT INTO user_signs ( userID, userSign ) "
     #                "VALUES ( %s, %s ) ON CONFLICT (userID) "
     #                "DO UPDATE SET userSign = %s", (user_id, sign, ))
+    print(user_id, sign)
     cursor.execute("INSERT INTO user_signs ( userID, userSign )"
                    "VALUES ( %s, %s ) ", (user_id, sign, ))
     connection.commit()
