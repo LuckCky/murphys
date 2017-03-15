b1c65ae68f2df48d507c184dd4ab26cbb5edf36e
LuckCky <www.aivanov@gmail.com>
2017-03-15 22:47:22 +0300

    debugging

--- dbhelper.py
+++ dbhelper.py
@@ -17,27 +17,27 @@ cursor = connection.cursor()
 
 try:
     cursor.execute("CREATE TABLE predictions ( date DATE, prediction VARCHAR(2000) ) ")
-    cursor.execute("CREATE TABLE user_signs ( userID VARCHAR(50), userSign VARCHAR(20) ) ")
+    cursor.execute("CREATE TABLE user_signs ( userID INTEGER, userSign VARCHAR(20) ) ")
     connection.commit()
 except:
     pass
-finally:
-    connection.close()
+# finally:
+#     connection.close()
 
 
-def conn():
-    connection = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port)
-    print('CONNECTION')
-    return connection.cursor(), connection
+# def conn():
+#     connection = psycopg2.connect(
+#         database=url.path[1:],
+#         user=url.username,
+#         password=url.password,
+#         host=url.hostname,
+#         port=url.port)
+#     print('CONNECTION')
+#     return connection.cursor(), connection
 
 
 def set_user_sign(user_id, sign):
-    cursor, connection = conn()
+    # cursor, connection = conn()
     cursor.execute("INSERT INTO user_signs ( userID, userSign ) VALUES ( %s, %s ) ON CONFLICT (userID) DO UPDATE SET userSign = %s", (user_id, sign, ))
     connection.commit()
 
@@ -58,7 +58,8 @@ def get_today_prediction(date):
 
 
 def get_user_sign(user_id):
-    cursor, connection = conn()
+    # cursor, connection = conn()
+    print('GOT CURSOR')
     cursor.execute("SELECT userSign FROM user_signs WHERE userID = %s", (user_id, ))
     sign = cursor.fetchone()
     print('SIGN', sign)
